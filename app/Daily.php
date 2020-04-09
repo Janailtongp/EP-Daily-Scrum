b7dc94a9be4b8c4f42811a8432978a56bc248e0b
Janailton Galvão Pereira <janailton@braso.com.br>
2020-04-09 10:28:33 -0300

    :lock: Creating register and authentication for user

--- app/Daily.php
+++ app/Daily.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Daily extends Model
 {
-    //
+    protected $fillable = [
+        'first_answer', 'second_answer', 'third_answer', 'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne('App\users');
+    }
 }
